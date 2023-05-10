7a7128053606c44b27781d8961b75fa896f32c5e
Mateus <mateus@Carlos-MacBook-Pro.local>
2023-05-10 14:29:20 -0300

    feat: create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Task.create!(name: "coxinha", description: "comprar na padaria", done: true)
+Task.create!(name: "paçoca", description: "comprar no mercado", done: false)
+Task.create!(name: "esfiha", description: "pedir no habbis", done: true)
+Task.create!(name: "marmita", description: "comprar na cabana", done: false)
